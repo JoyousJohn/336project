b42c9e65f1ed05e3bd75513fb296633a7573073c
Tatiana <tatiana.joli@outlook.com>
2024-05-03 09:57:55 -0400

    Color, font, and spacing edits to login page

--- js/explore.js
+++ js/explore.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $('.search-form').css('border-color', 'blue')
     })
     
-    // Remove search bar border when unfocussed
+    // Remove search bar border when unfocused
     $('.search-input').on('blur', function() {
         $('.search-form').css('border-color', 'gray')
     })
